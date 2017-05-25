44ad92b167d81c5de246ee4640a33d393a09da3e
Bruzhmelev Anton <bruzhmelev@gmail.com>
2017-05-25 20:09:48 +0300

    Полуготовое

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, Input, OnInit,  Output, ViewChild, ViewContainerRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/fromPromise';
 
+
 @Component({
   selector: 'gs-search',
   templateUrl: './search.component.html',
@@ -14,39 +15,41 @@ import 'rxjs/add/observable/fromPromise';
 })
 export class SearchComponent implements OnInit{
 
-@ViewChild('sinput', { read: ViewContainerRef})
-public sinput: ViewContainerRef;
+  @ViewChild('sinput', { read: ViewContainerRef})
+  public sinput: ViewContainerRef;
 
-private inputElem: HTMLInputElement;
+  @Input()
+  public searchResults: RedditSearchResItemData[];
 
-@Output()
-public search(value): void {
-  console.dir('sinput: ' + this.inputElem.value);
-}
 
-public ngOnInit(): void {
-  this.inputElem = this.sinput.element.nativeElement;
+  private inputElem: HTMLInputElement;
 
-  this.subscribeOnInput();
-}
+  @Output()
+  public search(value): void {
+    console.dir('sinput: ' + this.inputElem.value);
+  }
 
-private subscribeOnInput(): void {
-  Observable.fromEvent(this.inputElem, 'input')
-  .debounceTime(300)
-  .map((event: KeyboardEvent) => (event.target as HTMLInputElement).value)
-  // .do((searchTerm: string) => {
-  //   console.log(searchTerm);
-  // })
-  .switchMap((searchTerm: string) => {
-    return Observable
-      //.fromPromise(fetch(`https://api.github.com/search/repositories?q=${searchTerm}`)
-      .fromPromise(fetch(`https://www.reddit.com/r/gifs/search.json?q=${searchTerm}&sort=new`)
-      //https://www.reddit.com/r/gifs/search.json?q=ferrari&sort=new
-        .then((res: Response) => res.json()));
-  })
-  .map((res: any) => res.data.children)
-  .subscribe((items//: RedditSearchResItem[]
-  ) => console.dir(items));
-}//ferrari
+  public ngOnInit(): void {
+    this.inputElem = this.sinput.element.nativeElement;
 
+    this.subscribeOnInput();
+  }
+
+  private subscribeOnInput(): void {
+    Observable.fromEvent(this.inputElem, 'input')
+    .debounceTime(300)
+    .map((event: KeyboardEvent) => (event.target as HTMLInputElement).value)
+    .switchMap((searchTerm: string) => {
+      return Observable
+        .fromPromise(fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=new`)
+        //.fromPromise(fetch(`https://www.reddit.com/r/gifs/search.json?q=${searchTerm}&sort=new`)
+          .then((res: Response) => res.json()));
+          //ferrari
+    })
+    .map((res: any) => res.data ? res.data.children : [])
+    .subscribe((items: RedditSearchResItem[]) => {
+      console.dir(items);
+      this.searchResults = items.map((item: RedditSearchResItem) => item.data);
+    });
+  }
 }
--- types/redditSearchResItem/index.d.ts
+++ types/redditSearchResItem/index.d.ts
@@ -1,14 +1,20 @@
 type RedditSearchResItem = {
+  kind: string,
+  data: RedditSearchResItemData
+};
+
+type RedditSearchResItemData = {
   title: string,
   permalink: string,
   thumbnail: string,
   author: string,
   id: string,
   created: Date,
+  subreddit_name_prefixed: string,
+  url: string,
+  selftext_html: string,
+  selftext: string
   // preview?: Preview, //images[] - sorce.url
   // subreddit: string
-  // created_utc: Date,
-  // created_utc: Date,
-  // created_utc: Date,
-  // created_utc: Date,
 };
+
